b0bcaee2c58827169a36dbc14bdc52dc36f7f657
Primusz Péter <primuszp@gmail.com>
2018-03-21 14:28:20 +0100

    Add TextReader class

---
+++ Source/Primusz.SoilGenius/Primusz.SoilGenius.Core/IO/TxtReader.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+
+namespace Primusz.SoilGenius.Core.IO
+{
+    public class TxtReader : IDisposable
+    {
+        #region Members
+
+        private StreamReader reader;
+
+        #endregion
+
+        #region Properties
+
+        public char ColumnSeparator { get; set; }
+
+        public char DecimalSeparator { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public TxtReader(Stream stream)
+        {
+            if (stream != null)
+                reader = new StreamReader(stream);
+            else
+                throw new ArgumentNullException(nameof(stream));
+
+            ColumnSeparator = '\t';
+            DecimalSeparator = '.';
+        }
+
+        #endregion
+
+        public void Read(bool firstLineColumnNames = true)
+        {
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+
+                if (!string.IsNullOrEmpty(line))
+                {
+
+                }
+            }
+        }
+
+        #region IDisposable Interface
+
+        ~TxtReader()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (reader != null)
+                {
+                    reader.Dispose();
+                    reader = null;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Primusz.SoilGenius/Primusz.SoilGenius.Core/Model/TestPoint2d.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Primusz.SoilGenius.Core.Model
+{
+    class TestPoint2D
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+    }
+}
